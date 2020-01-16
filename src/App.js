bf0ce732193c911768dccd72a02d268cdc5d4988
Karen Bao <karenbao2021@u.northwestern.edu>
2020-01-16 12:13:55 -0600

    fifth slice

--- src/App.js
+++ src/App.js
@@ -2,60 +2,82 @@ import React, { useState, useEffect } from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
 
-const App = () => {
-  const [schedule, setSchedule] = useState({ title: '', courses: [] });
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
 
-  const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
 
-  const getCourseTerm = course => (
-    terms[course.id.charAt(0)]
-  );
+const Banner = ({ title }) => (
+  <h1 className="title">{ title }</h1>
+);
 
-  const getCourseNumber = course => (
-    course.id.slice(1, 4)
-  )
+const getCourseTerm = course => (
+  terms[course.id.charAt(0)]
+);
 
-  useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
-    }
-    fetchSchedule();
-  }, [])
+const getCourseNumber = course => (
+  course.id.slice(1, 4)
+)
 
-  const Banner = ({ title }) => (
-    <Title>{ title || '[loading...]' }</Title>
-  );
+const buttonState = selected => (
+  selected ? `button is-success is-selected` : 'button'
+);
 
-  const CourseList = ({ courses }) => (
-    <Button.Group>
-      {courses.map(course => <Course key={course.id} course={ course } />)}
-    </Button.Group>
-  );
+const TermSelector = ({ state }) => (
+  <div className="field has-addons">
+  { Object.values(terms)
+      .map(value => 
+        <button key={value}
+          className={ buttonState(value === state.term) }
+          onClick={ () => state.setTerm(value) }
+          >
+          { value }
+        </button>
+      )
+  }
+  </div>
+);
+  
+const Course = ({ course }) => (
+  <button className="button">
+    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+  </button>
+);
 
-  const Course = ({ course }) => (
-    <Button>
-      { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
-    </Button>
-  )
-  React.useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
-    }
-    fetchSchedule();
-  }, []);
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = React.useState('Fall');
+  const termCourses = courses.filter(course => term === getCourseTerm(course));
   
   return (
-    <Container>
+    <React.Fragment>
+      <TermSelector state={ { term, setTerm } } />
+      <div className="buttons">
+        { termCourses.map(course =>
+           <Course key={ course.id } course={ course }  />) }
+      </div>
+    </React.Fragment>
+  );
+};
+
+const App = () => {
+  const [schedule, setSchedule] = React.useState({ title: '', courses: [] });
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  React.useEffect(() => {
+    
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    
+    fetchSchedule();
+  }, []);
+
+  return (
+    <div className="container">
       <Banner title={ schedule.title } />
       <CourseList courses={ schedule.courses } />
-    </Container>
+    </div>
   );
 };
 
